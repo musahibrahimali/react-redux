aa46c27323d5e8e852e7eda0eda3f31192a063a1
musahibrahimali <musahibrahimali@gmail.com>
2021-11-12 18:35:44 +0000

    initial commit

--- src/app/App.js
+++ src/app/App.js
@@ -1,13 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 import { PostForm, Posts } from '../components/components';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
       <PostForm />
       <hr />
       <Posts />
